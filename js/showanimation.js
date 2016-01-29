c22c25e5826419d5c9d52b01af768d1317592f2d
ZQXie <2944927590@qq.com>
2016-01-29 21:04:53 +0800

    Revert "Revert "my2048""
    This reverts commit d79a785699be1e123ebc41945e3b453096122f5c.

---
+++ js/showanimation.js
@@ -0,0 +1,42 @@
+
+function showNumberWithAnimation(i, j, randNumber) {
+    var numberCell = $("#number-cell-"+i+"-"+j);
+    numberCell.css({
+        "background-color": getNumberBackgroundColor( randNumber ),
+        "color": getNumberColor( randNumber )
+    }).text( privateBoard[randNumber]).animate({
+        "width": cellSideLength,
+        "height": cellSideLength,
+        "top": getPosTop( i, j ),
+        "left": getPosLeft( i, j )
+    },20);
+}
+
+function showMoveAnimation(fromx, fromy, tox, toy){
+    var numberCell = $("#number-cell-" + fromx + "-" + fromy);
+    numberCell.animate({
+        "top": getPosTop(tox, toy),
+        "left": getPosLeft(tox, toy)
+    }, 200);
+    setBoardToLocal( boardLocalName , board );
+}
+
+function updateScore( score ){
+    if(board.length > 4){
+        board.pop();
+        board.push(score);
+    } else {
+        board.push(score);
+    }
+    if( noapace() ){
+        setBoardToLocal( boardLocalName , board);
+    }
+
+    $("#score").stop().animate({
+        'font-size': '40px'
+    },30,function(){
+        $(this).animate({
+            'font-size': '25px'
+        },60);
+    }).text(score);
+}
